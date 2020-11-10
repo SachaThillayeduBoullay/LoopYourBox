6dd644efc6c08ffb7604bf87f7d66fa304f831e2
Matt Nannetti <mattnannetti@gmail.com>
2020-11-10 15:53:01 +0100

    session update

--- controllers/user.js
+++ controllers/user.js
@@ -1,24 +1,26 @@
+const express = require('express');
+const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const joi = require('joi-oid');
+const nodemailer = require('nodemailer');
 
 
+exports.getAllUser = (req, res, next) => {
+    User.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({error}));
+    };
 
-exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-        const user = new User({
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            email: req.body.email,
-            password: hash
-        });
-        user.save()
-            .then(() => res.status(201).redirect('/index'))
-            .catch(error => res.status(400).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
+exports.getOneUser = (req, res, next) => {
+    User.findOne({_id:req.params.id})
+    .then(user => res.status(200).json(user))
+    .catch(error => res.status(404).json({error}));
 };
+
+
+
+
 
 
 exports.login = (req, res, next) => {
@@ -47,6 +49,130 @@ exports.login = (req, res, next) => {
 
 
 
+
+exports.signup = async (req, res, next) => {
+
+    const schema = joi.object().keys({
+        firstname: joi.string().trim().required(),
+        lastname: joi.string().trim().required(),
+        email: joi.string().trim().email().required(),
+        password: joi.string().pattern(new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')).required(), //1 upper, 1 lower, 1 number, 1 special char, 8 min length
+        confirmPassword: joi.any().equal(joi.ref('password'))
+        .required()
+        .label('Confirm password')
+        .messages({ 'any.only': '{{#label}} does not match' }),
+        status: joi.any().valid('member')
+    });
+
+    const result = schema.validate(req.body);
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+            const user = new User({
+            ...req.body,
+            password: hash
+        })
+    
+    user.save()
+      .then(() => res.status(201).redirect('/signup3/'))
+      .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+exports.updateUser = (req, res, next) => {
+    if (req.body.password) {
+        bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            //console.log(hash);
+            User.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id, password: hash})
+            .then(() => {
+                res.status(200).redirect('/myprofile');
+            })
+            .catch(error => res.status(400).json({ error }));
+            }
+        )
+        .catch(error => res.json({error}));
+    }
+
+    User.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+    .then(() => {
+        res.status(200).redirect('/myprofile');
+    })
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.deleteUser = (req, res, next) => {
+    User.deleteOne({_id: req.params.id})
+    .then(()=> res.status(200).json({message: 'Your account has been deleted'}))
+    .catch(error => res.status(400).json({error}));
+};
+
+
+
+exports.lostPwd = (req, res, next) => {
+
+    User.findOne({email:req.body.email})
+    .then(user => {
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // use SSL
+            auth: {
+              user: 'loopyourbox@gmail.com',
+              pass: 'loopyourbox'
+            }
+        });
+          
+        let mailOptions = {
+            from: 'contact@loopyourbox.com',
+            to: req.body.email,
+            subject: 'Password reset',
+            html: `
+            <h1>Loop Your Box</h1>
+            <p>Please click on the link to change your password: </p>
+            <p>
+                <a href="http://localhost:3000/changepwd?id=${user._id}">Change password</a>
+            </p>
+            `
+        };
+
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+              res.redirect('/');
+            }
+          });
+    })
+    .catch(error => res.status(404).json({error}));
+
+    
+};
+
+
+/*
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+        const user = new User({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: hash
+        });
+        user.save()
+            .then(() => res.status(201).redirect('/index'))
+            .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+};
+*/
 
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require("../middleware/auth");
 const userCtrl = require('../controllers/user');
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
+
+router.get('/user', auth, userCtrl.getAllUser);
+router.get('/user/:id', auth, userCtrl.getOneUser);
+router.put('/user/:id', userCtrl.updateUser);
+router.delete('/user/:id', auth, userCtrl.deleteUser);
+
+router.post('/lostpwd', userCtrl.lostPwd);
+
 
 module.exports = router;
