9ff35a2560d87956351d3511e137771d5aa36b73
SachaThillayeduBoullay <sachathillayeduboullay@gmail.com>
2020-11-13 09:19:11 +0100

    creation des test

---
+++ test/testTemp.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+// const ToTest = require(pathToTest);
+
+/*
+describe('description du test', () => {
+    it('dois retourne X', () => {
+           assert.equal(question, reponse);
+       });
+   });
+*/
+
+describe('description du test', () => {
+    it('dois retourne X', () => {
+           assert.equal(true, true);
+       });
+   });
