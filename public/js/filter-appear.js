65676d48b10ce04b302e1b9c445bb40c2a8b4f80
Lap Hoang <52751449+lap-hoang24@users.noreply.github.com>
2020-11-17 18:25:38 +0100

    qrcode page styled and linked

--- public/js/filter-appear.js
+++
@@ -1,6 +0,0 @@
-const idButton = document.getElementById('filter');
-const filterForm = document.getElementById('filter-form');
-
-idButton.addEventListener('click', () => {
-   filterForm.classList.toggle('appear');
-})
