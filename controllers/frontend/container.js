bdcb0caed7c23ae1d60cb543ba8f94f1e3656f14
Nathalie Havelange <havelange.nathalie@gmail.com>
2020-12-15 12:14:34 +0100

    bis

--- controllers/frontend/container.js
+++ controllers/frontend/container.js
@@ -9,21 +9,7 @@ exports.containerPage = async (req, res) => {
         let containerInfo = await fetch(url);
         containerInfo = await containerInfo.json();
 
-        const token = req.cookies["token"];
-        const decodedToken = jwt.verify(token, process.env.JWT_PW);
-        const userId = decodedToken.userId;
-        let urlUser = `${process.env.DOMAIN}/api/user/${userId}`;
-
-        let myInit = {
-            headers: {
-                'Authorization': 'Bearer ' + token
-            }
-        };
-
-        let userInfo = await fetch(urlUser, myInit);
-        userInfo = await userInfo.json();
-
-        res.render('pages/container/container', {containerInfo, userInfo})
+        res.render('pages/container/container', {containerInfo})
     } catch {
         res.status(401).render('pages/error',{ error: `Requête invalide`});
     }
@@ -39,10 +25,24 @@ exports.containerDetailsPage = async (req, res) => {
         let containerInfo = await fetch(url);
         containerInfo = await containerInfo.json();
 
+        const decodedToken = jwt.verify(token, process.env.JWT_PW);
+        const userId = decodedToken.userId;
+
+        let myInit = {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        };
+
+        let urlUser = `${process.env.DOMAIN}/api/user/${userId}`;
+
+        let userInfo = await fetch(urlUser, myInit);
+        userInfo = await userInfo.json();
+
         if (containerInfo.image != "noImage") {
             containerInfo.image = JSON.parse(containerInfo.image);
         }
-        res.render('pages/container/containerDetails', {containerInfo, status});
+        res.render('pages/container/containerDetails', {containerInfo, status, userInfo});
     } catch {
         res.status(401).render('pages/error',{ error: `Requête invalide`});
     }
